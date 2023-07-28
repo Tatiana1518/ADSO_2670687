7ba9688b54d9ca8a6be1bff599c0d7fc44e39cde
Tatiana1518 <citaosa321@gmail.com>
2023-07-28 15:28:17 -0500

    VisualStudioCode

---
+++ Javascript/ADSO_2670687-main/Ejercicio4/js/script4.js
@@ -0,0 +1,8 @@
+ /* Ejercicio 2
+ Suponga que un individuo desea invertir 
+ su capital en un banco y desea saber cuánto 
+ dinero ganara después de un mes si 
+ el banco paga a razón de 2% mensual.*/ 
+
+ let inv = prompt("Ingrese el valor de inversión");
+ let mes = prompt();
---
+++ js/js-main/Ejercicio1/js/script.js
@@ -0,0 +1,87 @@
+//console.log("Analisis y Desarrollo de Software");
+
+//alert("Analisis y Desarrollo de Software");
+
+// Para comentar en una sola linea de codigo
+
+/* este tipo de comentario 
+se trabaja en diferentes lineas
+de codigo*/
+
+//document.write("Analisis y Desarrollo de Software");
+
+//declarar variables var
+
+//var edad=19;
+
+//console.log("Mi edad es:"+edad);
+//alert("Mi edad es:"+edad);
+//document.write("Mi edad es:"+edad);
+//simbolo "+" va a funcionar concatenar 
+
+/* unir dos cadenas de texto
+const nom="Cindy"
+const ape='Ocampo'
+alert("mi nombre es "+nom+" y mi apellido es "+ape) */
+
+/*const num1=2
+const num2=2
+alert(num1+num2) "Suma correcto"
+alert("la suma es:" +num1+num2) "Suma Incorrecto"
+
+const suma = num1+num2
+parseInt(suma)                 "Correcto"
+alert("la suma es: "+suma)
+
+const num1=8
+const num2=2
+const resta = num1-num2
+alert("la resta es:" +resta)
+
+const num1=9
+const num2=5
+const mult = num1*num2
+alert("la multiplicacion es: " +mult)
+
+const num1=5
+const num2=5
+const div = num1/num2
+alert("la multiplicacion es: " +div) "Suma, Resta, Multiplicacion y Divicion"*/
+
+/*let nom=prompt("Escriba el nombre")
+alert("mi nombre es: "+nom)*/
+/*mostrar un alerta nombres y apellidos completos, edad, peso y dirección usando la funcion prompt */
+
+let nom=prompt("Escriba los nombres")   
+let ape=prompt("Escriba los apellidos")
+let edad=prompt("Escriba la edad")
+let peso=prompt("Escriba el peso")
+let direc=prompt("Escriba la direccion")
+alert("mis nombres son "+nom+", mis apellidos son "+ape+", mi edad es "+edad+ ", mi peso es "+peso+" y mi dirección es "+direc)
+
+//const edad2=21;
+
+//console.log("Mi edad es:"+edad2);
+//alert("Mi edad es:"+edad2);
+//document.write("Mi edad es:"+edad2);
+
+//let edad3;26
+
+//console.log("Mi edad es:"+edad3);
+//alert("Mi edad es:"+edad3);
+//document.write("Mi edad es:"+edad3);
+
+//let num=2.5;
+//console.log(typeof num);
+//booleano=true;
+//console.log(typeof booleano)
+//booleano='ADSO';
+//console.log(typeof booleano)
+//typeof es para definir tipos de datos
+//
+
+
+
+
+
+
